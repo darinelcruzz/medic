bcc63264b18da92a6408bcdad62841708ca40006
Darinel Cruz <darinelcruzz@gmail.com>
2018-02-07 19:46:53 -0600

    Vue js components copied!

--- resources/views/auth/admin/root.blade.php
+++ resources/views/auth/admin/root.blade.php
@@ -4,24 +4,25 @@
     @include('auth.admin.htmlhead')
 
     <body class="hold-transition skin-blue sidebar-mini">
-        <div class="wrapper">
-            @include('auth.admin.mainheader')
-            @include('auth.admin.sidebar')
+        <div id="app" v-cloak>
+            <div class="wrapper">
+                @include('auth.admin.mainheader')
+                @include('auth.admin.sidebar')
 
-            <div class="content-wrapper">
-                <section class="content-header">
-                    <h1>@stack('headerTitle')</h1>
-                </section>
+                <div class="content-wrapper">
+                    <section class="content-header">
+                        <h1>@stack('headerTitle')</h1>
+                    </section>
 
-                <!-- Main content -->
-                <section class="content container-fluid">
-                    @yield('content')
-                </section>
+                    <!-- Main content -->
+                    <section class="content container-fluid">
+                        @yield('content')
+                    </section>
+                </div>
+
+                @include('auth.admin.footer')
             </div>
-
-            @include('auth.admin.footer')
         </div>
-
         @include('auth.admin.scripts')
 
     </body>
--- resources/views/auth/index.blade.php
+++ resources/views/auth/index.blade.php
@@ -9,7 +9,18 @@
 @endpush
 
 @section('content')
-    <solid-box title="Una caja" color="primary">
-        <code>Hola mundo</code>
-    </solid-box>
+    <div class="row">
+        <div class="col-md-6">
+            <simple-box title="Una caja" color="primary">
+                <code>Hola mundo</code>
+                {!! Field::text('name', ['tpl' => 'withicon'], ['icon' => 'user']) !!}
+                {!! Field::text('last_name', ['tpl' => 'withicon'], ['icon' => 'user']) !!}
+                {!! Field::number('phone', ['tpl' => 'withicon'], ['icon' => 'user']) !!}
+                {!! Field::number('age', ['tpl' => 'withicon'], ['icon' => 'user']) !!}
+                {!! Field::text('address', ['tpl' => 'withicon'], ['icon' => 'user']) !!}
+                {!! Field::text('city', ['tpl' => 'withicon'], ['icon' => 'user']) !!}
+            </simple-box>
+        </div>
+    </div>
+
 @endsection
